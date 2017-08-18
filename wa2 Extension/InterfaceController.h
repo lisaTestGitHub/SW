b8699ee87fbb973500f9bf19eae792da3da3406e
local <local@lisasmacbookpro.local>
2017-08-18 14:23:59 -0700

    add watch

---
+++ wa2 Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  wa2 Extension
+//
+//  Created by local on 8/16/17.
+//  Copyright © 2017 local. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
---
+++ wa2 Extension/InterfaceController.m
@@ -0,0 +1,38 @@
+//
+//  InterfaceController.m
+//  wa2 Extension
+//
+//  Created by local on 8/16/17.
+//  Copyright © 2017 local. All rights reserved.
+//
+
+#import "InterfaceController.h"
+
+
+@interface InterfaceController ()
+
+@end
+
+
+@implementation InterfaceController
+
+- (void)awakeWithContext:(id)context {
+    [super awakeWithContext:context];
+
+    // Configure interface objects here.
+}
+
+- (void)willActivate {
+    // This method is called when watch view controller is about to be visible to user
+    [super willActivate];
+}
+
+- (void)didDeactivate {
+    // This method is called when watch view controller is no longer visible
+    [super didDeactivate];
+}
+
+@end
+
+
+
